47c26e63cc484f1dda507d93261172cdd7bc5139
Patriota Alba <62809129+patriotaalba@users.noreply.github.com>
2020-10-23 17:00:57 +0700

    adding js on about menu

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ const line2 = document.querySelector('.burger .line-dua');
 const line3 = document.querySelector('.burger .line-tiga');
 const burgerDiv = document.querySelector('.burger div')
 const resNav = document.querySelector('.res-nav');
+const about = document.querySelector('.about')
+const aboutNav = document.querySelector('.right-side ul li:nth-child(4)')
+const aboutResNav = document.querySelector('.res-nav ul li:nth-child(4)')
+const closeBtn = document.querySelector('.close-btn')
+const body = document.querySelector('body')
+
 
 burger.addEventListener("click", function(){
     resNav.style.transform = "translateX(0%)";
@@ -18,3 +24,15 @@ resNav.addEventListener("click", function(){
     line2.style.transform = "scale(1)";
     line3.style.transform = "scale(1)";
 });
+
+aboutNav.addEventListener("click", function() {
+    about.style.transform = "scale(1)"
+})
+
+aboutResNav.addEventListener("click", function() {
+    about.style.transform = "scale(1)"
+})
+
+closeBtn.addEventListener("click", function() {
+    about.style.transform = "scale(0)"
+})
